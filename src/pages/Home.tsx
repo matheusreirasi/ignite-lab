d668b905eb56f12de317a9874366e04042df2f85
Matheus Felipe <matheus.reirasi@gmail.com>
2022-08-18 10:10:09 -0300

    added padding on the sides of description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ export function Home (): JSX.Element {
                     <h1 className="mt-8 text-[40px] leading-tight">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed px-2">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
